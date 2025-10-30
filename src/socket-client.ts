5e02840eb4be7b7e67a37ad9df551603f05aa767
Bruno Jimenez <omarjm0910@gmail.com>
2025-10-29 22:03:10 -0600

    mostramos un mensaje de conectado/desconectado

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,7 +1,17 @@
-import { Manager } from "socket.io-client";
+import { Manager, Socket } from "socket.io-client";
 
 export const connectToServer = () => {
   const manager = new Manager("http://localhost:3000/socket.io/socket.io.js");
   const socket = manager.socket("/");
-  
+  addListeners(socket);
+};
+
+const addListeners = (socket: Socket) => {
+  const serverStatusLaberl = document.querySelector("#server-status")!;
+  socket.on("connect", () => {
+    serverStatusLaberl.innerHTML = 'connected';
+  });
+  socket.on("disconnect", () => {
+    serverStatusLaberl.innerHTML = 'disconnected';
+  });
 };
