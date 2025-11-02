10aeb325a8c402ec47bafdde9b619547c57f6998
Bruno Jimenez <omarjm0910@gmail.com>
2025-11-02 16:35:44 -0600

    enviamos mensajes desde el cliente

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -9,6 +9,9 @@ export const connectToServer = () => {
 const addListeners = (socket: Socket) => {
   const serverStatusLaberl = document.querySelector("#server-status")!;
   const clientsUl = document.querySelector("#clients-ul")!;
+  const messageForm = document.querySelector<HTMLFormElement>("#message-form");
+  const messageInput =
+    document.querySelector<HTMLInputElement>("#message-input");
 
   socket.on("connect", () => {
     serverStatusLaberl.innerHTML = "connected";
@@ -24,5 +27,15 @@ const addListeners = (socket: Socket) => {
       clientsHtml += `<li>${client}</li>`;
     });
     clientsUl.innerHTML = clientsHtml;
+  });
+
+  messageForm?.addEventListener("submit", (event) => {
+    event.preventDefault();
+    if (messageInput?.value.trim().length <= 0) return;
+    socket.emit("message-from-client", {
+      id: "YO!!!",
+      message: messageInput?.value,
+    });
+    messageInput.value = "";
   });
 };
